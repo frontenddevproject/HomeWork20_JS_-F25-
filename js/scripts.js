b116fe2f2209eb80e8c692222687fd7478fb73b1
frontenddevproject <frontenddevproject@gmail.com>
2023-03-26 15:33:52 +0300

    create new repozitory

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+const data = [
+	{
+		"name": "Abraham Marsh",
+		"phone": "1-843-901-9977",
+		"email": "morbi@google.org"
+	},
+	{
+		"name": "Isabella Mcgowan",
+		"phone": "1-874-933-8532",
+		"email": "ac.feugiat@protonmail.couk"
+	},
+	{
+		"name": "Leandra Levy",
+		"phone": "(482) 150-2683",
+		"email": "vitae.dolor@hotmail.couk"
+	},
+	{
+		"name": "Shana Byrd",
+		"phone": "1-134-348-2696",
+		"email": "sit.amet@hotmail.edu"
+	},
+	{
+		"name": "Victoria Vazquez",
+		"phone": "(676) 677-3028",
+		"email": "mauris.magna@yahoo.ca"
+	}
+];
